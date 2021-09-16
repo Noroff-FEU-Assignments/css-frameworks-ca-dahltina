2dd3eea70cd267c121025f9fe9799a8e2a1b6790
Tina Dahl <tina@e5r.no>
2021-09-16 17:59:51 +0100

    Move nav to separate component

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,31 +1,54 @@
 import React from 'react'
+import { Switch, Route, NavLink } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Form from "react-bootstrap/Form";
 import Formcontrol from "react-bootstrap/Formcontrol";
 import Button from "react-bootstrap/Button";
+import Home from './Home';
+import News from './news/News';
+import Contact from './contact/Contact';
 
 function Navigation() {
   return (
     <div>
-       <Navbar bg="light" expand="lg">
+       <Navbar bg="light" expand="lg" style={{ minHeight: '70px' }}>
         <Container>
-          <Navbar.Brand href="#home">The YAY Company</Navbar.Brand>
+          <Navbar.Brand href="/" >The YAY Company</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#news">News</Nav.Link>
-              <Nav.Link href="#contact">Contact</Nav.Link>
+              <NavLink exact to="/" className="nav-link">Home</NavLink>
+              <NavLink to="/news" className="nav-link">News</NavLink>
+              <NavLink to="/contact" className="nav-link">Contact</NavLink>
             </Nav>
-            <Form inline>
+            <Form className="d-flex">
               <Formcontrol type="text" placeholder="Search" className="mr-sm-2" />
-              <Button variant="outline-success">Search</Button>
+              <Button variant="outline-success">Go</Button>
             </Form>
           </Navbar.Collapse>
         </Container>
       </Navbar>
+
+       {/*
+        A <Switch> looks through all its children <Route>
+        elements and renders the first one whose path
+        matches the current URL. Use a <Switch> any time
+        you have multiple routes, but you want only one
+        of them to render at a time
+      */}
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/news">
+          <News />
+        </Route>
+        <Route path="/contact">
+          <Contact />
+        </Route>
+      </Switch>
     </div>
   )
 }
